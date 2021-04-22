787dd055a2674ff32d0eae58d838702b4ab4b311
Omar Mendoza <59121837+mendoza000@users.noreply.github.com>
2021-04-22 13:42:38 -0400

    igualdad

---
+++ firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+		apiKey: "AIzaSyB87E35LF7M-C2CYhhMJNB_nXjkZ7phFXE",
+		authDomain: "fire-money-7c3ce.firebaseapp.com",
+		projectId: "fire-money-7c3ce",
+		storageBucket: "fire-money-7c3ce.appspot.com",
+		messagingSenderId: "855462612566",
+		appId: "1:855462612566:web:5a2df90c6083fc289d75f7"
+		};
+firebase.initializeApp(firebaseConfig);
